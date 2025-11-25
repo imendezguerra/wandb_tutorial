546b86dd75a5c7c67b3f5f744f1dc67da8b8f8b5
imendezguerra <irene.mendez17@imperial.ac.uk>
2025-11-25 19:49:21 +0000

    Added automatic model query and loading

--- src/model.py
+++ src/model.py
@@ -1,7 +1,10 @@
 """Network"""
 
+import random
+import numpy as np
 import torch
 from torch import nn
+from contextlib import contextmanager
 
 class MLP(nn.Module):
     def __init__(self, input_dim=2, hidden_dim=64, output_dim=2, dropout=0.0):
@@ -28,4 +31,62 @@ def get_device():
     else:
         device = torch.device('cpu')
     return device
+
+def set_random_seed(rand_seed):
+    """Set random seeds in places that use random generators."""
+    # Python random
+    random.seed(rand_seed)
+    # NumPy
+    np.random.seed(rand_seed)
+    # PyTorch CPU
+    torch.manual_seed(rand_seed)
+    # PyTorch GPU (CUDA)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(rand_seed)
+        torch.cuda.manual_seed_all(rand_seed)
+
+
+@contextmanager
+def mode_check(mode):
+    if mode == "train":
+        with torch.enable_grad():
+            yield
+    else:  # "eval", "test", "validation"
+        with torch.no_grad():
+            yield
+
+
+def run_one_epoch(model, loader, optimizer, mode):
+    # Set model mode
+    if mode:
+        model.train()
+    else:
+        model.eval()
     
+    # Initialise variables
+    total_loss = 0.0
+    correct = 0
+    total = 0
+
+    with mode_check(mode):
+        for step, (X_batch, y_batch) in enumerate(loader):
+
+            # Run model for every batch
+            logits = model(X_batch)
+            loss = nn.functional.cross_entropy(logits, y_batch)
+
+            # Compute gradients and update model
+            if mode == 'train':
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            # Compute loss
+            total_loss += loss.item() * X_batch.size(0)
+            preds = logits.argmax(dim=1)
+            correct += (preds == y_batch).sum().item()
+            total += X_batch.size(0)
+
+    avg_loss = total_loss / total
+    acc = correct / total
+    return avg_loss, acc
